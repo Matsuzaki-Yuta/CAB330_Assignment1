7a34befd310abbd3991e51c52136b895271ef16a
Matsuzaki-Yuta <n11761776@qut.edu.au>
2026-08-17 17:52:12 +1000

    Add Task 1 EDA and preprocessing

---
+++ evaluation/evaluation.py
@@ -0,0 +1,12 @@
+# evaluation.py
+# │
+# ├── model performance metrics
+# │   ├── accuracy
+# │   └── classification report
+# │
+# ├── plots
+# │   └── ROC curve
+# │
+# └── model comparison
+#     ├── training vs test performance
+#     └── compare models
